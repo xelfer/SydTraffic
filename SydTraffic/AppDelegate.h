478f7314ea8c448b73f7ac399c483a030d228fb7
Nicolas Triantafillou <nick@oz.org>
2013-09-21 15:45:38 +1000

    Initial Commit

---
+++ SydTraffic/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SydTraffic
+//
+//  Created by Nicolas Triantafillou on 21/09/13.
+//  Copyright (c) 2013 Nicolas Triantafillou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SydTraffic/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SydTraffic
+//
+//  Created by Nicolas Triantafillou on 21/09/13.
+//  Copyright (c) 2013 Nicolas Triantafillou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
